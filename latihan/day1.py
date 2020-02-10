6dfc5ea6c531f36bd15f6ae0008de9f2f718e921
mega <sheismega@gmail.com>
2020-02-10 11:13:32 +0700

    'latihan'

---
+++ latihan/day1.py
@@ -0,0 +1,32 @@
+# Variable
+
+# nama = 'Mega' # string
+# tanggal_lahir = 29 # numerical
+# status = True # Boolean
+
+print(type(nama))
+print(type(tanggal_lahir))
+print(type(status))
+
+nama = input("Siapa nama kamu? :")
+umur = input("Masukan umur :")
+jeniskelamin = input("Laki-laki / Perempuan :")
+pekerjaan = input("pekerjaan kamu? :")
+
+print(nama) ;
+print(umur) ;
+print(jeniskelamin) ;
+print(pekerjaan) ;
+
+nama = input("Nama kamu? : ") ;
+umur = input("Umur kamu? : ") ;
+jeniskelamin = input("Jenis kelamin kamu? : ") ;
+pekerjaan = input("pekerjaan kamu? : ") ;
+
+print("nama : " + nama) ;
+print("umur : "  + umur);
+print("Jenis kelamin : " + jeniskelamin) ;
+print("pekerjaan : " + pekerjaan) ;
+
+
+
